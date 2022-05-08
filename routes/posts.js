21c82d483f1eec1133de4d71abf648aa15d79bb7
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-08 19:44:37 +0530

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,18 @@ router.post("/", async (req, res) => {
   }
 });
 // update post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId !== req.body.userId) {
+      return res.status(401).json("you can update only your post");
+    }
+    await post.updateOne({ $set: req.body });
+    res.status(200).json("The post is updated");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // delete a post
 // like a post
 // comment a post
