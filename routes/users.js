8dd66e321f9587ba6c2870339fc6a4a2f1b2eafa
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-08 13:04:28 +0530

    feat:update user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("hey it's user route");
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account updated successfully");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
 });
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
