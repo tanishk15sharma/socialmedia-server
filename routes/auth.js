a6291a252a5185b9800cf263526df2ef236df818
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-08 11:57:47 +0530

    feat : pass bcrypt , signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 // register
 
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "tansssaaassddd22",
-    email: "jon@gamilaa.comssaass22",
-    password: "12345699sssss22",
-  });
+router.post("/register", async (req, res) => {
+  try {
+    // this will create new password
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  await user.save();
-  res.send("ok!!!");
+    // create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // store data/user
+    const user = await newUser.save();
+    res.status(200).json(user);
+    await user.save();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
