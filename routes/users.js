d1c412599e2554d0c722951c3763c351538a2741
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-08 13:31:12 +0530

    feat:delete and get user

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,29 @@ router.put("/:id", async (req, res) => {
   }
 });
 // delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted successfully");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
+
 // get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 // follow a user
 // unfollow a user
 
