ce3fe17b30c9ad17990892d8fe537b5c16ce600f
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-08 12:23:48 +0530

    feat: login route

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,26 @@ router.post("/register", async (req, res) => {
     res.status(200).json(user);
     await user.save();
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
     console.log(err);
+    res.status(500).json(err);
   }
 });
 
